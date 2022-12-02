cf65fe66cf29730301fdb60255415c544e78561a
Lazzzer <dev@lazarpavicevic.ch>
2022-12-02 15:48:47 +0100

    feat: first try at udp clients and servers

--- internal/client/client.go
+++ internal/client/client.go
@@ -1,1 +1,54 @@
 package client
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+type Client struct {
+	Address   string
+	Addresses []string
+}
+
+func (c *Client) Run() {
+	udpAddr, err := net.ResolveUDPAddr("udp4", c.Address)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	connection, err := net.DialUDP("udp4", nil, udpAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("The UDP server is %s\n", connection.RemoteAddr().String())
+	defer connection.Close()
+
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print(">> ")
+		text, _ := reader.ReadString('\n')
+		data := []byte(text + "\n")
+
+		_, err = connection.Write(data)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if strings.TrimSpace(string(data)) == "STOP" {
+			fmt.Println("Exiting UDP client!")
+			return
+		}
+
+		buffer := make([]byte, 1024)
+		n, _, err := connection.ReadFromUDP(buffer)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Reply: %s\n", string(buffer[0:n]))
+	}
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,57 @@
 package server
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type Server struct {
+	Address   string
+	Addresses []string
+}
+
+func (s *Server) Run() {
+	udpAddr, err := net.ResolveUDPAddr("udp4", s.Address)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	connection, err := net.ListenUDP("udp4", udpAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer connection.Close()
+	buffer := make([]byte, 1024)
+	rand.Seed(time.Now().Unix())
+
+	for {
+		n, addr, err := connection.ReadFromUDP(buffer)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Print("-> ", string(buffer[0:n-1]))
+
+		if strings.TrimSpace(string(buffer[0:n])) == "STOP" {
+			fmt.Println("Exiting UDP server!")
+			return
+		}
+
+		data := []byte(strconv.Itoa(101))
+		fmt.Printf("data: %s\n", string(data))
+		_, err = connection.WriteToUDP(data, addr)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
