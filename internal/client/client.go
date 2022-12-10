35886f4f6cf4934676e84b4ead2da137d8e28eba
Marinlestylo <chil.tfm@gmail.com>
2022-12-10 21:27:01 +0100

    feat: handle ctrl + c

--- internal/client/client.go
+++ internal/client/client.go
@@ -7,8 +7,10 @@ import (
 	"log"
 	"net"
 	"os"
+	"os/signal"
 	"strconv"
 	"strings"
+	"syscall"
 
 	"github.com/Lazzzer/labo3-sdr/internal/shared"
 	"github.com/Lazzzer/labo3-sdr/internal/shared/types"
@@ -18,12 +20,25 @@ type Client struct {
 	Servers map[int]string
 }
 
-var running = true
+var exitChan = make(chan os.Signal, 1) // Catch du CTRL+C
+
+var invalidCommand = "invalid command"
+var wrongServerNumber = "invalid server number"
+var emptyInput = "empty input"
+var chargeMustBePositive = "charge must be a positive integer"
 
 func (c *Client) Run() {
+	signal.Notify(exitChan, syscall.SIGINT)
+
+	go func() {
+		<-exitChan
+		fmt.Println("Good bye!")
+		os.Exit(0)
+	}()
+
 	fmt.Println("SDR - Labo 3 - Client")
 	reader := bufio.NewReader(os.Stdin)
-	for running {
+	for {
 		displayPrompt()
 		input, err := reader.ReadString('\n')
 		if err != nil {
@@ -39,16 +54,14 @@ func (c *Client) Run() {
 
 		go sendCommand(command, servAddr)
 	}
-	fmt.Println("Good bye!")
 }
 
 func displayPrompt() {
-	fmt.Println("Available commands:")
+	fmt.Println("\nAvailable commands:")
 	fmt.Println("  - <server number> add <number>")
 	fmt.Println("  - <server number> ask")
 	fmt.Println("  - <server number> new")
 	fmt.Println("  - <server number> stop")
-	// TODO: handle ctrl+c
 	fmt.Println("  - quit")
 	fmt.Println("Enter a command to send to a connected server on the network:")
 }
@@ -58,54 +71,54 @@ func processInput(input string, c *Client) (string, string, error) {
 
 	// String vide
 	if len(args) == 0 {
-		return "", "", fmt.Errorf("empty input")
+		return "", "", fmt.Errorf(emptyInput)
 	}
 
 	// Quit
 	if args[0] == string(types.Quit) {
-		running = false
-		return "", "", fmt.Errorf("client quitting program")
-		// TODO: Refactor, should not return an error, we can reuse ctrl+c logic with a signal channel
+		exitChan <- syscall.SIGINT
 	}
 
-	if len(args) < 2 {
-		return "", "", fmt.Errorf("missing command")
+	if len(args) < 2 || len(args) > 3 {
+		return "", "", fmt.Errorf(invalidCommand)
 	}
 
 	// Vérification du numéro du serveur
 	srvNumber, err := strconv.Atoi(args[0])
 	if err != nil || srvNumber < 1 || srvNumber > len(c.Servers) {
-		return "", "", fmt.Errorf("invalid server number")
+		return "", "", fmt.Errorf(wrongServerNumber)
 	}
 
-	// Vérification de la commande
+	// Vérification des commandes
 	command := types.Command{Value: nil}
-	switch args[1] {
-	case string(types.Add):
-		if len(args) != 3 {
-			return "", "", fmt.Errorf("invalid add command")
-		}
+	if len(args) == 3 && args[1] == string(types.Add) { // Vérification de la commande ask
 		value, err := strconv.Atoi(args[2])
-		if err != nil || value <= 0 {
-			return "", "", fmt.Errorf("invalid add command")
+		if err == nil && value >= 0 {
+			command.Type = types.Add
+			command.Value = &value
+		} else {
+			return "", "", fmt.Errorf(chargeMustBePositive)
 		}
-		command.Type = types.Add
-		command.Value = &value
-	case string(types.Ask):
-		command.Type = types.Ask
-	case string(types.New):
-		command.Type = types.New
-	case string(types.Stop):
-		command.Type = types.Stop
-	default:
-		return "", "", fmt.Errorf("unknown command")
+	} else if len(args) == 2 { // Vérification des autres commandes
+		switch args[1] {
+		case string(types.Ask):
+			command.Type = types.Ask
+		case string(types.New):
+			command.Type = types.New
+		case string(types.Stop):
+			command.Type = types.Stop
+		default:
+			return "", "", fmt.Errorf(invalidCommand)
+		}
+	} else {
+		return "", "", fmt.Errorf(invalidCommand)
 	}
 
 	// Création du json
 	if jsonCommand, err := json.Marshal(command); err == nil {
 		return string(jsonCommand), c.Servers[srvNumber], nil
 	} else {
-		return "", "", fmt.Errorf("invalid command")
+		return "", "", fmt.Errorf("error while marshalling command")
 	}
 }
 
@@ -120,7 +133,12 @@ func sendCommand(command string, address string) {
 		shared.Log(types.ERROR, err.Error())
 		return
 	}
-	defer connection.Close()
+	defer func(connection *net.UDPConn) {
+		err := connection.Close()
+		if err != nil {
+			shared.Log(types.ERROR, err.Error())
+		}
+	}(connection)
 
 	_, err = connection.Write([]byte(command + "\n"))
 	if err != nil {
